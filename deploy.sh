e48f7708bc9aed3d1581249b034ca8a2be318c8f
Manjunath <manjunathdevops30@gmail.com>
2025-02-16 15:51:51 +0000

    Updating the deploy.sh file

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 DOCKER_HUB_USERNAME="manjunathdc"
 IMAGE_NAME="devops-app"
 TAG="dev"
-SERVER_IP="54.202.75.111"
+SERVER_IP="35.95.23.135"
 SSH_KEY="oregon-test.pem"
 
 # SSH into the server and deploy the application
